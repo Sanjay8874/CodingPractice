e8269d5fb5ccbf520f71de3520634f4b3dc7ac1e
Sanjay Yadav <sanjay8874002308@gmail.com>
2024-05-21 11:28:17 +0530

    Solve Leetcode problem

--- src/LeetCode/LeetCodemain.java
+++ src/LeetCode/LeetCodemain.java
@@ -1,9 +1,19 @@
 package LeetCode;
+import LeetCode.Solutions;
 
 public class LeetCodemain {
 
 	public static void main(String[] args) {
 		
+		String s = "abcde";
+		String t = "edbac";
+		
+		int sum = Solutions.findPermutationDifference(s, t);
+		//System.out.println(sum);
+		
+		String[] strings = {"d","b","c","b","c","a"};
+		int k=2;
+		System.out.println(Solutions.kthDistinct(strings, k));
 	
 	
 	}
--- src/LeetCode/Solutions.java
+++ src/LeetCode/Solutions.java
@@ -1,30 +1,90 @@
 package LeetCode;
 
-public class Solutions {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        
-       
-        int[] prefixProducts = new int[n];
-        int[] suffixProducts = new int[n];
- 
-        prefixProducts[0] = 1;
-        for (int i = 1; i < n; i++) {
-            prefixProducts[i] = prefixProducts[i - 1] * nums[i - 1];
-        }
-        
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
-        suffixProducts[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            suffixProducts[i] = suffixProducts[i + 1] * nums[i + 1];
-        }
-        
-        // Calculate final result
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            result[i] = prefixProducts[i] * suffixProducts[i];
-        }
-        
-        return result;
-    }
+import javax.swing.border.MatteBorder;
+
+public class Solutions {
+	public int[] productExceptSelf(int[] nums) {
+		int n = nums.length;
+
+		int[] prefixProducts = new int[n];
+		int[] suffixProducts = new int[n];
+
+		prefixProducts[0] = 1;
+		for (int i = 1; i < n; i++) {
+			prefixProducts[i] = prefixProducts[i - 1] * nums[i - 1];
+		}
+
+		suffixProducts[n - 1] = 1;
+		for (int i = n - 2; i >= 0; i--) {
+			suffixProducts[i] = suffixProducts[i + 1] * nums[i + 1];
+		}
+
+		// Calculate final result
+		int[] result = new int[n];
+		for (int i = 0; i < n; i++) {
+			result[i] = prefixProducts[i] * suffixProducts[i];
+		}
+
+		return result;
+	}
+
+	public static int findPermutationDifference(String s, String t) {
+		
+		String[] s1 = s.split("");
+		String[] t1 = t.split("");
+		
+		int l1 = s1.length;
+		int l2 = t1.length;
+		int sum = 0;
+		
+		for(int i=0;i<l1;i++) {
+			for(int j=0;j<l2;j++) {
+				if(s1[i].equals(t1[j])) {
+					sum = Math.abs(sum + Math.abs(i-j));
+					
+					
+				}
+				
+			}
+		}
+		
+		return sum;
+	}
+	
+	
+	
+	 //2053. Kth Distinct String in an Array
+	 public static String kthDistinct(String[] arr, int k) {
+		 
+		 Map<String, Integer> mapStr = new HashMap<String, Integer>();
+		 
+		 for(String str : arr) {
+			 
+			 mapStr.put(str, mapStr.getOrDefault(str, 0)+1);
+			 
+		 }
+		 
+	
+		 List<String> list = new ArrayList<String>();
+		 
+		 for(String s : arr) {
+			 if(mapStr.get(s)==1) {
+				 list.add(s);
+			 }	
+		 }
+		 if(k>list.size()) {
+			 return "";
+		 }
+		 else {
+			return list.get(k-1);
+		}
+	        
+	      
+	    }
 }
